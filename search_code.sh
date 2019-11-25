fb18c637599aeeabdaf8ba9d768b8c45ed83dd70
keisuke ohashi <kog71903@gmail.com>
2019-11-25 21:35:04 +0900

    [update]search_codeをちょっと便利に

--- search_code.sh
+++ search_code.sh
@@ -2,9 +2,9 @@
 
 # オプションでファイル指定とかもできたらいいかも
 if [ $# == 1 ]; then
-    cat ~/Programming/CodingMemo/* | rg --context 3 $1
+    rg --context 3 $1 ~/Programming/CodingMemo/ 
 elif [ $# == 2 ]; then
-    cat ~/Programming/CodingMemo/* | rg --context 3 $1 | rg --context 3 $2
+    rg --context 3 $1 ~/Programming/CodingMemo | rg --context 3 $2
 elif [ $# == 3 ]; then
-    cat ~/Programming/CodingMemo/* | rg --context 3 $1 | rg --context 3 $2 | rg --context 3 $3
+    rg --context 3 $1 ~/Programming/CodingMemo | rg --context 3 $2 | rg --context 3 $3
 fi
